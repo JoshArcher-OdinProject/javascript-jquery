4388004f3e05a8f62554392a4a27ba5bdd52826b
Josh Archer <josh@jamail.co.uk>
2017-03-22 22:02:39 +0000

    Completes Javascript and jQuery project

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 
 // The number of squares per side of the grid at initial build
 var squaresPerSide = 30
-var randomColour = false
 
-// The needs to be defined as null to enable resetting the grid and retain user
-// defined grid size. If grid is reset before user defines a size without this
+// These need to be defined as null to enable resetting grid and retain user
+// defined grid size. If grid is reset before user defines a size, without this
 // declaration newGridSize is simply undefined. 
 var newGridSize = null
+
+// Similarly, defines rgb as null to avoid undefined before user has selected
+// the Random Colour button. 
+var rgb = null
 
 // Call functions in this section
 // ==============================
@@ -20,13 +23,27 @@ buildGrid(squaresPerSide);
 // =======================================
 
 // Select drawing colour
-$("#selectColour").click(selectColour);
+$("#setColourBlack").click(function () {
+    // Need to set to True to cause if statement to return else in randomColor()
+    // This allows us to preserve drawLine() colour through grid resets
+    setBlack = true
+    drawLine();
+});
 
+// Random drawing colour
+$("#randomColour").click(function () {
+    // Need to set to false to cause if statement to run in randomColor()
+    // This allows us to preserve drawLine() colour through grid resets
+    setBlack = false
+    randomColour()
+    drawLine()
+});
+   
 // Select grid size
 $("#gridSize").click(changeGridSize);
 
 // Reset the grid
-$('#resetGrid').click(resetGrid);
+$('#clearGrid').click(clearGrid);
 
 // Define function definitions in this section
 // ===========================================
@@ -62,15 +79,26 @@ function buildGrid (squaresPerSide) {
     drawLine();
 }
 
-// Detect a mouseover event and change the square colour to black
+// Detect a mouseover event and change the square colour
 function drawLine () {
-    $('.grid-square').mouseover(function() {
-        $(this).css('background-color', 'black');
-    })
+    // Checks if 'Random Colour' has been selected. If yes, preserves it.
+    // We use setBlack boolean to preserve drawLine() state through grid
+    // resets. 
+    if (rgb !== null && setBlack == false) {
+        $('.grid-square').mouseover(function() {
+            $(this).css('background-color', randomColour());
+        })
+    }
+    // Sets standard line colour to black
+    else {
+        $('.grid-square').mouseover(function() {
+            $(this).css('background-color', 'black');
+        })
+    }
 }
 
-// Reset the grid. Delete all squares, rebuild and activate draw line
-function resetGrid () {
+// Reset the grid. Keeps the reset grid size to user entered size if given
+function clearGrid () {
     if ( newGridSize !== null) {
         buildGrid(newGridSize);
     }
@@ -85,12 +113,24 @@ function changeGridSize () {
     if ( newGridSize == null || newGridSize == '' || isNaN(newGridSize) == true ) {
         alert("Erm...that's not a number. Please enter a number!");
         }
-    else if (newGridSize > 400) {
+    else if (newGridSize > 250) {
         alert("Yikes, that number is too large, you'll crash the browser! Pick something less than 400.")
         }
     else {
         buildGrid(newGridSize);
     }
 }
+
+// Allow user to make colour of line random
+function randomColour () {
+    // Set variables to number between 0 and 255
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    // Create a string matching rbg(000,000,000) for use in CSS
+    rgb = "rgb(" + r + "," + g +"," + b + ")"
+    return rgb;
+}
+
 
 
